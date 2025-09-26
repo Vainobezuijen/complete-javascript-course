d553e46c8504f573c6d6a9d77de130b213c26d5d
vaino <144139208+Vainobezuijen@users.noreply.github.com>
2025-09-26 16:05:01 +0200

    idk

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,29 @@
+let js = 'amazing';
+
+console.log(40 + 8);
+
+let country = 'netherlands';
+let continent = 'europe';
+let population = 100;
+
+console.log(country, continent, population);
+
+let jsIsFun = true;
+console.log(typeof jsIsFun);
+console.log(typeof true);
+console.log(typeof 2);
+
+jsIsFun = 'OUI';
+
+const birthYear = 2001;
+const ageNow = 2025 - 2001;
+const firstName = 'Väinö';
+const lastName = 'Bezuijen';
+const fullName = firstName + ' ' + lastName;
+
+population++;
+
+let x = 5;
+let y = 6;
+console.log(x >= y);
+const isFullAge = ageNow >= 18;
